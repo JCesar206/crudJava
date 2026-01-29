ce3ab4398c6aa2121d8adf1f65282b24825da127
Julio Cesar <jcesar206@hotmail.com>
2026-01-29 15:46:04 -0600

    Se sube archivo nuevo I18n para manejo de idiomas ES,EN

--- src/app/util/I18n.java
+++ src/app/util/I18n.java
@@ -1,4 +1,19 @@
-package app;
+package app.util;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class I18n {
+	private static Locale locale = new Locale("es");
+	private static ResourceBundle bundle =
+		ResourceBundle.getBundle("i18n.messages", locale);
+	
+	public static void setLanguage(String lang) {
+		locale = new Locale(lang);
+		bundle = ResourceBundle.getBundle("i18n.messages", locale);
+	}
+	
+	public static String t(String key) {
+		return bundle.getString(key);
+	}
 }
